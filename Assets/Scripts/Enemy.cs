eff3c8bf752b5a5c6e7394cffe440a7e2888d2f8
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-10-01 09:28:30 -0400

    enemy script and main script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+
+    public float speed = 10f;      // The speed in m/s
+
+    public float fireRate = 0.3f;  // Seconds/shot (Unused)
+
+    public float health = 10;
+
+    public int score = 100;      // Points earned for destroying this
+
+    private BoundsCheck bndCheck;
+
+    // This is a Property: A method that acts like a field
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+    public Vector3 pos
+    {                                                     // a
+
+
+        get
+        {
+
+            return (this.transform.position);
+
+        }
+
+        set
+        {
+
+            this.transform.position = value;
+
+        }
+
+    }
+
+
+
+    void Update()
+    {
+
+        Move();
+
+        if (bndCheck != null && bndCheck.offDown)
+        {
+            if (pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+
+
+                Destroy(gameObject);
+            }
+
+
+        }
+    }
+
+
+
+    public virtual void Move()
+    {                                             // b
+
+        Vector3 tempPos = pos;
+
+        tempPos.y -= speed * Time.deltaTime;
+
+        pos = tempPos;
+
+    }
+}

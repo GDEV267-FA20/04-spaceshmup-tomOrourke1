fea8a99c8872b9bc8df41f2dd913def74428e677
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-10-04 11:59:13 -0400

    added more enemy scripts
    working on the 3rd enemy script

---
+++ Assets/Scripts/Enemies/Enemy_1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1 : Enemy
+{
+
+    [Header("Set in Inspector: Enemy_1")]
+    //# seconds for a full sine wave
+    public float waveFrequency = 2f;
+
+    //sinewave widthin meters
+    public float waveWidth = 4f;
+    public float waveRotY = 45f;
+
+    private float x0;
+    private float birthTime;
+
+
+    private void Start()
+    {
+        x0 = pos.x;
+
+        birthTime = Time.time;
+    }
+
+
+    public override void Move()
+    {
+        // cause pos is a property, you can't directly set pos.x
+        // so get the pos ad an editable vector 3
+        Vector3 tempPos = pos;
+
+        //theta adjusts basxed on time
+        float age = Time.time - birthTime;
+        float theta = Mathf.PI * 2 * age / waveFrequency;
+        float sin = Mathf.Sin(theta);
+        tempPos.x = x0 + waveWidth * sin;
+        pos = tempPos;
+
+        //rotate a bit about y
+        Vector3 rot = new Vector3(0, sin * waveRotY, 0);
+        this.transform.rotation = Quaternion.Euler(rot);
+
+        //Base.Move() still handles the movement down in y
+        base.Move();
+
+        //print(bndCheck.isOnScreen);
+    }
+
+
+}
---
+++ Assets/Scripts/Enemies/Enemy_3.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_3 : Enemy
+{
+    // Enemy_3 will move following a Bezier curve, which is a linear
+    //   interpolation between more than two points.
+
+    [Header("Set in Inspector: Enemy_3")]
+    public float lifeTime = 5f;
+
+    [Header("Set Dynamically: Enemy_3")]
+    public Vector3[] points;
+    public float birthTime;
+
+    private void Start()
+    {
+        points = new Vector3[3];
+
+        points[0] = pos;
+
+        //set min and max
+        float xMin = -bndCheck.camWidth + bndCheck.radius;
+        float xMax = bndCheck.camWidth + bndCheck.radius;
+
+        Vector3 v;
+
+        //random middle pos
+        v = Vector3.zero;
+        v.x = Random.Range(xMin, xMax);
+        v.y = -bndCheck.camHeight * Random.Range(2.75f, 2);
+
+        points[1] = v;
+
+        //random final pos
+        v = Vector3.zero;
+        v.y = pos.y;
+        v.x = Random.Range(xMin, xMax);
+
+        points[2] = v;
+
+        //set birthtime
+        birthTime = Time.time;
+    }
+
+
+
+
+    public override void Move()
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
